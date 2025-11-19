1835b664814dffe7a6fb564f2558d847f303c6ce
Jyothika <jyothikakalla0651@gmail.com>
2025-11-19 23:48:06 +0530

    changed endpoint

--- src/main/java/com/example/test/Controller/ProductController.java
+++ src/main/java/com/example/test/Controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")  // Base URL
+@RequestMapping("/api/list")  // Base URL
 public class ProductController {
 
     @Autowired
